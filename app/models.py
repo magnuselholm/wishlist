248ce8c35404a96a9ac836f368868058fe3ef5f8
magnuselholm <meje23@student.aau.dk>
2026-06-12 23:25:53 +0200

    added fields

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 import sqlite3
 from config import Config
 
+### Database funktioner
+
 def get_db():
     db = sqlite3.connect("ønsker.db")
     db.row_factory = sqlite3.Row
@@ -11,7 +13,10 @@ def init_db():
     db.execute("""
         CREATE TABLE IF NOT EXISTS ønsker (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            ønske TEXT NOT NULL
+            ønske TEXT NOT NULL,
+            pris TEXT,
+            link TEXT,
+            billede TEXT
         )
     """)
     db.commit()
@@ -22,7 +27,7 @@ def hent_ønsker():
 
 def tilføj_ønske(tekst):
     db = get_db()
-    db.execute("INSERT INTO ønsker (ønske) VALUES (?)", (tekst,))
+    db.execute("INSERT INTO ønsker (ønske, pris, link, billede) VALUES (?, ?, ?, ?)", (tekst, pris, link, billede))
     db.commit()
 
 def slet_ønske(id):
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, redirect, request, render_template
 from app.models import hent_ønsker, tilføj_ønske, slet_ønske
 
+### HTTP endpoints
+
 bp = Blueprint("main", __name__)
 
 @bp.route("/")
@@ -11,8 +13,11 @@ def index():
 @bp.route("/tilføj", methods=["POST"])
 def tilføj():
     ønske = request.form.get("ønske")
+    pris = request.form.get("pris")
+    link = request.form.get("link")
+    billede = request.form.get("billede")
     if ønske:
-        tilføj_ønske(ønske)
+        tilføj_ønske(ønske, pris, link, billede)
     return redirect("/")
 
 @bp.route("/slet/<int:id>", methods=["POST"])
